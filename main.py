229d04b80f85ffbaa70a1bec708dc17357190f98
M J <69307802+whitehat-1@users.noreply.github.com>
2022-04-18 07:20:58 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+
+from fastapi import Form
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class RegisterSchema(BaseModel):
+    username:  str
+    password: str
+
+
+@app.post("/home")
+async def home():
+    return {"message": "Welcome to Twitter"}
+
+
+
+@app.post("/login/")
+async def login():
+    return{"login":"enter your login details here"}
+
+## Twitter signup form ##
+
+@app.post("/signup/")
+async def signup(name: str = Form(...),
+ emailaddress: str = Form(...), 
+ Dateofbirth: str = Form(...),
+ phonenumber: int = Form(...)):
+    return {"name": name, 
+    "emailaddress": emailaddress, 
+    "Dateofbirth": Dateofbirth, 
+    "phonenumber": phonenumber
+    }
+
+
+
+
+
+
+
